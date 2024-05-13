1cebf18f8e3b29440f8dd62bfb0cdb95d5b92f95
MengyaoLin <mengyao.lin@student.adelaide.edu.au>
2024-05-14 01:41:48 +0930

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {routeMap} from "../routeMap";
 import styles from './Header.module.css';
-import Typography from '@mui/material/Typography';
+import {Toolbar, Button,Typography} from "@mui/material";
+import bannerImage from '../images/course3.jpeg';
 
 
 const headerContent = [
@@ -26,20 +27,27 @@ const headerContent = [
 function Header() {
     return (
         <div>
-            <div className={styles.header}>
-                <div className={styles.logo}>logo</div>
+        <div className={styles.container}>
+            <Toolbar className={styles.toolbar}>
+                <div className={styles.logoPlaceholder} />
                 <div className={styles.menu}>
                     {headerContent.map(item => (
-                        <a className={styles.link} href={item.url}>
-                            <span>{item.title}</span>
-                        </a>
+                        <Button color="inherit" href={item.url}>
+                           {item.title}
+                        </Button>
                     ))}
                 </div>
-                <a href="/login" className={styles.login}>Login</a>
-                <a href="/register" className={styles.login}>Register</a>
-                <a href="/personal-information" className={styles.login}>Info</a>
+                <div className={styles.login}>
+                    <Button  variant="text" href={routeMap.login}>Login</Button>
+                    <Button  variant="text" href={routeMap.register}>Register</Button>
+                    <Button  variant="text" href={routeMap.personalInfo}>Info</Button>
+                </div>
+            </Toolbar>
+        </div>
+            <div className={styles.media}>
+                <img src={bannerImage} alt="Banner" />
+                <Typography variant="h3" component="p">Youth AI Learning Platform</Typography>
             </div>
-            <div className={styles.content}><Typography variant="h3">Youth AI learning platform</Typography></div>
         </div>
     );
 }
